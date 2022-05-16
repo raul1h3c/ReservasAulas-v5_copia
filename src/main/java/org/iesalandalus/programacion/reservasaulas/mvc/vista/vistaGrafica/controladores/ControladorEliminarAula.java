1b8650b19a5865582783fbdc9d9a74540c45bae0
Raúl Herrerías Corral <raulhc1313@gmail.com>
2022-05-16 12:25:18 +0200

    Aula inicial

---
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/vista/vistaGrafica/controladores/ControladorEliminarAula.java
@@ -0,0 +1,16 @@
+package org.iesalandalus.programacion.reservasaulas.mvc.vista.vistaGrafica.controladores;
+
+import org.iesalandalus.programacion.reservasaulas.mvc.controlador.IControlador;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ControladorEliminarAula {
+		
+	private IControlador controladorPrincipal;
+	
+	public void setControladorPrincipal(IControlador controlador) {
+		this.controladorPrincipal = controlador;
+	}
+}
--- src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/vista/vistaGrafica/controladores/ControladorPrincipal.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/vista/vistaGrafica/controladores/ControladorPrincipal.java
@@ -18,11 +18,14 @@ import javafx.stage.Stage;
 public class ControladorPrincipal {
 
 	
-	@FXML private Button BTSalirPrincipal,BTInsertarAula;
+	@FXML private Button BTSalirPrincipal,BTInsertarAula,BTEliminarAula,BTListarAula,BTConsultarDisponibilidad;
 	@FXML private MenuItem MICerrar;
 	
-	private Stage insertarAulaStage;
+	private Stage insertarAulaStage, eliminarAulaStage, listarAulaStage, consultarDisponibilidadStage;
 	private ControladorInsertarAula controllerInsertarAula;
+	private ControladorEliminarAula controllerEliminarAula;
+	private ControladorListarAula controllerListarAula;
+	private ControladorConsultarDisponibilidad controllerConsultarDisponibilidad;
 	
 	
 	private IControlador controladorPrincipal;
@@ -31,32 +34,50 @@ public class ControladorPrincipal {
 	}
 	
 	
-	
-	private void crearVentanaInsertarAula() throws IOException {
-		if (insertarAulaStage == null) {
-			insertarAulaStage = new Stage();
-			FXMLLoader loader = new FXMLLoader(LocalizadorRecursos.class.getResource("vistas/InsertarAula.fxml"));
-			AnchorPane raiz = loader.load();
-			controllerInsertarAula = loader.getController();
-			controllerInsertarAula.setControladorPrincipal(controladorPrincipal);
-			
-			Scene insertarAulas = new Scene(raiz);
-			insertarAulaStage.setTitle("Insertar aulas");
-			insertarAulaStage.initModality(Modality.APPLICATION_MODAL);
-			insertarAulaStage.setScene(insertarAulas);
-		} else {
-		}
-	}
-	
 	@FXML
 	public void abrirInsertarAula() throws IOException {
 		crearVentanaInsertarAula();
 		insertarAulaStage.showAndWait();
-		
+	}
+	
+	@FXML 
+	public void abrirEliminarAula() throws IOException {
+		crearVentanaEliminarAula();
+		eliminarAulaStage.showAndWait();
 	}
 	
 	@FXML
 	private void salir(ActionEvent evento) {
 		((Stage) BTSalirPrincipal.getParent().getScene().getWindow()).close();
 	}
+	
+	private void crearVentanaInsertarAula() throws IOException {
+			insertarAulaStage = new Stage();
+			FXMLLoader loader = new FXMLLoader(LocalizadorRecursos.class.getResource("vistas/InsertarAula.fxml"));
+			AnchorPane raiz = loader.load();
+			controllerInsertarAula = loader.getController();
+			controllerInsertarAula.setControladorPrincipal(controladorPrincipal);
+			
+			Scene scene = new Scene(raiz,500,400);
+			insertarAulaStage.setTitle("Insertar aulas");
+			insertarAulaStage.initModality(Modality.APPLICATION_MODAL);
+			insertarAulaStage.setScene(scene);
+		} 
+	
+	private void crearVentanaEliminarAula() throws IOException {
+		if (eliminarAulaStage == null) {
+			eliminarAulaStage = new Stage();
+			FXMLLoader loader2 = new FXMLLoader(LocalizadorRecursos.class.getResource("vistas/EliminarAula.fxml"));
+			AnchorPane raiz2 = loader2.load();
+			controllerEliminarAula = loader2.getController();
+			controllerEliminarAula.setControladorPrincipal(controladorPrincipal);
+			
+			Scene scene2 = new Scene(raiz2,500,400);
+			insertarAulaStage.setTitle("Eliminar aulas");
+			insertarAulaStage.initModality(Modality.APPLICATION_MODAL);
+			insertarAulaStage.setScene(scene2);
+		} else {
+		}
+	}
+	
 }
